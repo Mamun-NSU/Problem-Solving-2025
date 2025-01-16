5004c40e78e80e248c9b468ad41b55c6834f82ea
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2025-01-16 22:46:46 +0600

    857A - Array Coloring solve successfully

---
+++ codeforces/Problemset/1857A - Array Coloring.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int t; // Number of test cases
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n; // the length of the array a
+
+        // vector<int> a(n);
+        int a[n]; //
+
+        // If we find the number of ODD elements of "a" is even, then result YES
+        int oddCounter = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+
+            if (a[i] % 2 == 1)
+            {
+                oddCounter++; // Count the ODD elements
+            }
+        }
+
+        if (oddCounter % 2 == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
