ee050d9d92136ecba7bd3c1d9df4dd2eee461833
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2025-01-20 23:53:42 +0600

    1950A - Stair, Peak, or Neither solve successfully

---
+++ codeforces/Problemset/1950A - Stair, Peak, or Neither.cpp
@@ -0,0 +1,32 @@
+// Link: https://codeforces.com/problemset/problem/1950/A
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if (a < b && b < c)
+        {
+            cout << "STAIR" << endl;
+        }
+        else if (a < b && b > c)
+        {
+            cout << "PEAK" << endl;
+        }
+        else
+        {
+            cout << "NONE" << endl;
+        }
+    }
+
+    return 0;
+}
