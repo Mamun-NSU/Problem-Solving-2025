6ae5d9f14bbaef6d74daf90f665fdcf570e7afcf
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2025-03-05 23:47:27 +0600

    2579. Count Total Number of Colored Cells solve successfully

---
+++ leetcode/Daily Question/2579. Count Total Number of Colored Cells.cpp
@@ -0,0 +1,10 @@
+// Link: https://leetcode.com/problems/count-total-number-of-colored-cells/description/
+
+class Solution
+{
+public:
+    long long coloredCells(int n)
+    {
+        return 1 + 4LL * n * (n - 1) / 2;
+    }
+};
