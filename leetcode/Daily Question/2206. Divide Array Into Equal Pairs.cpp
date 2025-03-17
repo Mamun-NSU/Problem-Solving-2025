4295a109f7c3f29175823df510a6fa2c91b4be0b
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2025-03-17 23:43:39 +0600

    2206. Divide Array Into Equal Pairs solve successfully

---
+++ leetcode/Daily Question/2206. Divide Array Into Equal Pairs.cpp
@@ -0,0 +1,24 @@
+// Link: https://leetcode.com/problems/divide-array-into-equal-pairs/description/
+
+class Solution
+{
+public:
+    bool divideArray(vector<int> &nums)
+    {
+        unordered_map<int, int> counter;
+        for (int num : nums)
+        {
+            counter[num]++;
+        }
+
+        for (const auto &[_, count] : counter)
+        {
+            if (count % 2 != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
