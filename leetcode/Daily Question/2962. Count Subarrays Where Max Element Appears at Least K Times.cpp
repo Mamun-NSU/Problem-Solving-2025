76ca546ff10543c0a1b4e717763906e1a82d7292
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2025-04-29 23:56:41 +0600

    2962. Count Subarrays Where Max Element Appears at Least K Times solve successfully

---
+++ leetcode/Daily Question/2962. Count Subarrays Where Max Element Appears at Least K Times.cpp
@@ -0,0 +1,31 @@
+// Link: https://leetcode.com/problems/count-subarrays-where-max-element-appears-at-least-k-times/description/
+
+class Solution
+{
+public:
+    long long countSubarrays(vector<int> &nums, int k)
+    {
+        long long count = 0, ans = 0;
+        int n = nums.size(), j = 0, maxi = INT_MIN;
+        for (int i : nums)
+            maxi = max(maxi, i);
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] == maxi)
+                count++;
+            if (count == k)
+                ans += n - i;
+            while (count == k)
+            {
+                j++;
+                if (nums[j - 1] == maxi)
+                {
+                    count--;
+                    continue;
+                }
+                ans += n - i;
+            }
+        }
+        return ans;
+    }
+};
