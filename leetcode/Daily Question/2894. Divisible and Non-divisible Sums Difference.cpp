1d7139810eb82e9bdf32879a2d8a340b20a81e49
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2025-05-27 23:25:45 +0600

    2894. Divisible and Non-divisible Sums Difference solve successfully

---
+++ leetcode/Daily Question/2894. Divisible and Non-divisible Sums Difference.cpp
@@ -0,0 +1,13 @@
+// Link: https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/description/
+
+class Solution
+{
+public:
+    int differenceOfSums(int n, int m)
+    {
+        int sum1 = 0, sum2 = 0;
+        for (int num = 1; num <= n; num++)
+            num % m ? sum1 += num : sum2 += num;
+        return sum1 - sum2;
+    }
+};
