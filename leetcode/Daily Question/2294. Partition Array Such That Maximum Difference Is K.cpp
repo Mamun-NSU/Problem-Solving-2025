c0cc7356b1d4f0cf48d21e320e2a6e2866896de6
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2025-06-19 23:59:38 +0600

    2294. Partition Array Such That Maximum Difference Is K solve successfully

---
+++ leetcode/Daily Question/2294. Partition Array Such That Maximum Difference Is K.cpp
@@ -0,0 +1,22 @@
+// Link: https://leetcode.com/problems/partition-array-such-that-maximum-difference-is-k/description/
+
+class Solution
+{
+public:
+    int partitionArray(vector<int> &nums, int k)
+    {
+        sort(nums.begin(), nums.end()); // 1. sort
+        int i = 0, n = nums.size(), groups = 0;
+
+        while (i < n)
+        {                                     // 3. process one group
+            int limit = nums[i] + k;          //   smallest + k
+            i = upper_bound(nums.begin() + i, //   first index > limit
+                            nums.end(),
+                            limit) -
+                nums.begin();
+            ++groups; //   one group formed
+        }
+        return groups; // 4. answer
+    }
+};
