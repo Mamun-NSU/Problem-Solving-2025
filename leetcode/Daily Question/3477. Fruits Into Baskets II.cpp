3b4854dbbb99adb49de82cf661cecf34fcdb4575
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2025-08-05 23:54:57 +0600

    3477. Fruits Into Baskets II solve successfully

---
+++ leetcode/Daily Question/3477. Fruits Into Baskets II.cpp
@@ -0,0 +1,23 @@
+// Link: https://leetcode.com/problems/fruits-into-baskets-ii/description/
+
+class Solution
+{
+public:
+    int numOfUnplacedFruits(vector<int> &fruits, vector<int> &baskets)
+    {
+        int n = fruits.size(), alloted = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if (fruits[i] <= baskets[j])
+                {
+                    baskets[j] = -1; // mark basket as used
+                    alloted++;
+                    break;
+                }
+            }
+        }
+        return n - alloted;
+    }
+};
